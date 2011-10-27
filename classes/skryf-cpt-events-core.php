692c9bdc0748ac8ba09f20f8271a1fd0fd8347fe
Morten Hauan <morten@hauan.me>
2011-10-27 10:12:50 +0200

    [touch:3] {t:15} Created custom taxonomies

--- classes/skryf-cpt-events-core.php
+++ classes/skryf-cpt-events-core.php
@@ -4,7 +4,11 @@ class SkryfCptEvents {
 	
 	function __construct() {
 		
-		$this->init_cpt(); // Init custom post type
+		 // Init custom post type
+		$this->init_cpt();
+		
+		// Init taxonomies
+		$this->init_ctax();
 		
 	}
 	
@@ -45,4 +49,59 @@ class SkryfCptEvents {
 				
 	}
 	
+	private function init_ctax() { // Create taxonomies
+	
+		// Make new Category taxonomy for event
+		$labels = array(
+			'name' => _x( 'Categories', 'taxonomy general name', 'skryf-cpt-events' ),
+			'singular_name' => _x( 'Category', 'taxonomy singular name', 'skryf-cpt-events' ),
+			'search_items' =>  __( 'Search Categories', 'skryf-cpt-events' ),
+			'all_items' => __( 'All Categories', 'skryf-cpt-events' ),
+			'parent_item' => __( 'Parent Category', 'skryf-cpt-events' ),
+			'parent_item_colon' => __( 'Parent Category:', 'skryf-cpt-events' ),
+			'edit_item' => __( 'Edit Category', 'skryf-cpt-events' ), 
+			'update_item' => __( 'Update Category', 'skryf-cpt-events' ),
+			'add_new_item' => __( 'Add New Category', 'skryf-cpt-events' ),
+			'new_item_name' => __( 'New Category Name', 'skryf-cpt-events' ),
+			'menu_name' => __( 'Category', 'skryf-cpt-events' ),
+		); 	
+		
+		register_taxonomy('skryf_cpt_events_category',array('skryf_cpt_events'), array(
+			'hierarchical' => true,
+			'labels' => $labels,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'event-category' ),
+		));
+		
+		// Make Tags taxonomy for event
+		$labels = array(
+			'name' => _x( 'Tags', 'taxonomy general name', 'skryf-cpt-events' ),
+			'singular_name' => _x( 'Tag', 'taxonomy singular name', 'skryf-cpt-events' ),
+			'search_items' =>  __( 'Search Tags', 'skryf-cpt-events' ),
+			'popular_items' => __( 'Popular Tags', 'skryf-cpt-events' ),
+			'all_items' => __( 'All Tags', 'skryf-cpt-events' ),
+			'parent_item' => null,
+			'parent_item_colon' => null,
+			'edit_item' => __( 'Edit Tag', 'skryf-cpt-events' ), 
+			'update_item' => __( 'Update Tag', 'skryf-cpt-events' ),
+			'add_new_item' => __( 'Add New Tag', 'skryf-cpt-events' ),
+			'new_item_name' => __( 'New Tag Name', 'skryf-cpt-events' ),
+			'separate_items_with_commas' => __( 'Separate tags with commas', 'skryf-cpt-events' ),
+			'add_or_remove_items' => __( 'Add or remove tags', 'skryf-cpt-events' ),
+			'choose_from_most_used' => __( 'Choose from the most used tags', 'skryf-cpt-events' ),
+			'menu_name' => __( 'Tags', 'skryf-cpt-events' ),
+		); 
+		
+		register_taxonomy('skryf_cpt_events_tag','skryf_cpt_events',array(
+			'hierarchical' => false,
+			'labels' => $labels,
+			'show_ui' => true,
+			'update_count_callback' => '_update_post_term_count',
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'event-tag' ),
+		));	
+
+	}
+
 }
